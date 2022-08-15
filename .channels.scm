d152f1e1c506f35a47dd1552faebbd50fcb05e31
Peter Polidoro <peter@polidoro.io>
2022-08-15 16:16:35 -0400

    Add guix files

---
+++ .channels.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "2365e8435a13c3aa4308e88773de0a0e7b9ffef9")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-janelia)
+        (url "https://github.com/guix-janelia/guix-janelia.git")
+        (branch "main")
+        (commit
+          "d4cd10bde2b81aaeb29ac4bc8fc3160683b7cbc2")))
---
+++ .manifest.scm
@@ -0,0 +1,7 @@
+(specifications->manifest
+ '("kicad"
+   "kicad-doc"
+   "kicad-symbols"
+   "kicad-footprints"
+   "kicad-packages3d"
+   "kicad-templates"))
